cbbaeeae33a54125642c8c2bd1eaa02b427cb370
Andrew Laws <andrew.laws@iinet.net.au>
2019-06-11 17:16:31 +0800

    Updated batch files to poll Federation Manager for a good time to start up

--- src/java/ucef-java-genx-pong/run-pong-federate.bat
+++ src/java/ucef-java-genx-pong/run-pong-federate.bat
@@ -1,16 +1,37 @@
 @ECHO OFF
 
+set JAVA_MAIN_CLASS="gov.nist.hla.genx.GenxPongFederate"
+
 set MVN=
 for /f "delims=" %%i in ('where.exe mvn') do @set MVN="%%i"
-call :DequotedEcho %MVN%
-
 IF %MVN% == "" (
     call :DequotedEcho "The `mvn` (Maven) application could not be found. Ensure it is installed and placed in your PATH."
     EXIT /B
 ) ELSE (
-    %MVN% exec:java -Dexec.mainClass="gov.nist.hla.genx.GenxPongFederate" -Dexec.args="%*"
+    call :WaitForFederationManager
+    %MVN% exec:java -Dexec.mainClass="%JAVA_MAIN_CLASS%" -Dexec.args="%*"
 )
 goto :eof
+
+:WaitForFederationManager
+    setlocal enabledelayedexpansion
+    REM 'ping' the HTTP server on the federation manager  
+    REM to determine when it's a good time to start  
+    set CURL_RESPONSE=""
+    for /f "delims=" %%i in ('curl -s http://localhost:8080/query/is-waiting-for-federates') do @set CURL_RESPONSE="%%i"
+    if %CURL_RESPONSE% == "true" (
+        REM ready to go
+        EXIT /B
+    )
+    call :DequotedEcho "The Federation Manager does not seem to be ready yet..."
+    TIMEOUT /t 5 /NOBREAK
+    if ERRORLEVEL 1 (
+        REM if the user presses CTRL+C we want to exit completely
+        EXIT /B
+        goto :eof
+    )
+    REM keep looping
+    goto :WaitForFederationManager
 
 :DequotedEcho
     setlocal
